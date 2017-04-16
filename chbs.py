a5e53231b10c6b1de9374aaa581ce50502eceb68
Hal Peterson <vobine@users.noreply.github.com>
2017-04-16 10:07:22 -0500

    Add CLI

--- chbs.py
+++ chbs.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 from random import choice
+from argparse import ArgumentParser
 
 class Dictionary:
     def __init__ (self, infile):
@@ -10,6 +11,25 @@ class Dictionary:
     def choose (self, n=4):
         return [choice (self.words) for i in range (n)]
 
+def main (argv):
+    """CLI."""
+    parser = ArgumentParser (
+        'CHBS:  generate a passphrase from random words.')
+    parser.add_argument ('--dictionary', '-d',
+                         action='store',
+                         default='/usr/share/dict/cracklib-small',
+                         help='List of words to choose from')
+    parser.add_argument ('--length', '-l',
+                         action='store',
+                         type=int,
+                         default=4,
+                         help='Number of words in passphrase')
+
+    args = parser.parse_args (argv)
+
+    dd = Dictionary (args.dictionary)
+    print ('Random: {0:s}'.format (','.join (dd.choose (args.length))))
+
 if __name__ == "__main__":
-    dd = Dictionary ('/usr/share/dict/cracklib-small')
-    print ('Random: {0:s}'.format (','.join (dd.choose ())))
+    from sys import argv
+    main (argv[1:])
