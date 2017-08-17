9a91b7c0829601e73ab5cca31f101642094b9ae7
Hal Peterson <vobine@users.noreply.github.com>
2017-08-17 05:45:03 -0500

    Add length limits on individual words and the string as a whole. *NB* the string-length limit doesn't work yet.
    Add a parameter to select the number of words in the string.

--- chbs.py
+++ chbs.py
@@ -4,12 +4,18 @@ from random import choice
 from argparse import ArgumentParser
 
 class Dictionary:
-    def __init__ (self, infile):
+    def __init__ (self, infile, limit=100):
         with open (infile, 'rt') as inf:
-            self.words = [ll.strip () for ll in inf]
+            self.words = tuple (ll.strip () for ll in inf if len (ll) <= limit + 1)
 
-    def choose (self, n=4):
-        return [choice (self.words) for i in range (n)]
+    def choose (self, n=4, total=1000):
+        result = []
+        for _ in range (n):
+            word = choice (self.words)
+            total -= len (word) + 1
+            result.append (word)
+
+        return result
 
 def main (argv):
     """CLI."""
@@ -22,13 +28,23 @@ def main (argv):
     parser.add_argument ('--length', '-l',
                          action='store',
                          type=int,
+                         default=100,
+                         help='Maximum length of each word in passphrase')
+    parser.add_argument ('--total', '-t',
+                         action='store',
+                         type=int,
+                         default=1000,
+                         help='Maximum number of characters in passphrase')
+    parser.add_argument ('--words', '-w',
+                         action='store',
+                         type=int,
                          default=4,
                          help='Number of words in passphrase')
 
     args = parser.parse_args (argv)
 
-    dd = Dictionary (args.dictionary)
-    print ('Random: {0:s}'.format (','.join (dd.choose (args.length))))
+    dd = Dictionary (args.dictionary, args.length)
+    print ('Random: {0:s}'.format (','.join (dd.choose (args.words, args.total))))
 
 if __name__ == "__main__":
     from sys import argv
