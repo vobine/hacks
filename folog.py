7def61ef5a55e5428cbed6c9370d4e91a29042e5
Hal Peterson <vobine@users.noreply.github.com>
2017-11-21 05:46:27 -0600

    First cut at class-ification of Google Sheets Python example.

---
+++ folog.py
@@ -0,0 +1,60 @@
+"""Hacking foodlog.  Read-only spreadsheet access.
+Partly lifted from Google API quickstart.py."""
+
+import httplib2
+import os
+from apiclient import discovery
+from oauth2client import client, tools
+from oauth2client.file import Storage
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Food log hackery'
+
+class Folog:
+    """Food logging."""
+
+    def __init__ (self):
+        pass
+
+    def authorize (self):
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir,
+                                       'sheets.googleapis.com-python-quickstart.json')
+
+        store = Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+            flow.user_agent = APPLICATION_NAME
+            if flags:
+                credentials = tools.run_flow(flow, store, flags)
+            else: # Needed only for compatibility with Python 2.6
+                credentials = tools.run(flow, store)
+            print('Storing credentials to ' + credential_path)
+        self.credentials =  credentials
+
+    def discover (self):
+        """Shows basic usage of the Sheets API.
+
+        Creates a Sheets API service object and prints the names and majors of
+        students in a sample spreadsheet:
+        https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+        """
+        http = self.credentials.authorize(httplib2.Http())
+        discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                        'version=v4')
+        self.service = discovery.build('sheets', 'v4', http=http,
+                                       discoveryServiceUrl=discoveryUrl)
+
+    def get (self):
+        spreadsheetId = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+        rangeName = 'Class Data!A2:E'
+        result = self.service.spreadsheets().values().get(
+            spreadsheetId=spreadsheetId, range=rangeName).execute()
+        self.values = result.get('values', [])
